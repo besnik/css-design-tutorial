a6415c604a69183d106f7cb6402db47b1e720875
Topper <skippovic@gmail.com>
2023-04-18 19:19:10 +0200

    refactoring structure of repo

---
+++ 301-vue-components/02-component-in-component/component-app.js
@@ -0,0 +1,18 @@
+import CustomBtn from './component-button.js'
+
+export default {
+
+  setup() {
+
+  },
+
+  components: {
+    CustomBtn
+  },
+
+  template: `
+    <CustomBtn/>
+    <CustomBtn/>
+    <CustomBtn/>
+  `
+}
---
+++ 301-vue-components/02-component-in-component/component-button.js
@@ -0,0 +1,13 @@
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const count = ref(0)
+    return { count }
+  },
+  template: `
+    <button @click="count++">
+      You clicked me {{ count }} times.
+    </button>`
+  // or `template: '#my-template-element'`
+}
